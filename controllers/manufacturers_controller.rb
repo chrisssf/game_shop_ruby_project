9a9546bb0313c76d986764f61a94fa4978eefe26
Chris Fraser <chrisssfraser@gmail.com>
2019-12-16 11:15:52 +0000

    add a manufacturer working

--- controllers/manufacturers_controller.rb
+++ controllers/manufacturers_controller.rb
@@ -8,7 +8,19 @@ get '/manufacturers' do
   erb ( :"manufacturers/index" )
 end
 
+get '/manufacturers/new' do
+  # @products = Product.all()
+  # @manufacturers = Manufacturer.all()
+  erb(:"manufacturers/new")
+end
+
 get '/manufacturers/:id' do
   @manufacturer = Manufacturer.find(params[:id].to_i)
   erb(:"manufacturers/show")
+end
+
+post '/manufacturers' do
+  @manufacturer = Manufacturer.new(params)
+  @manufacturer.save()
+  erb(:"manufacturers/create")
 end
